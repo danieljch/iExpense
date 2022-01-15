a48c8754112c0542ddab63f2c21b25d09d41f2a0
Daniel JCH <danielj.callisayah@gmail.com>
2022-01-15 16:15:20 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 15/1/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
