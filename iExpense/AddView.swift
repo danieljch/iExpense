218658e5d29226cea09f0d92ecf5c7e5a6888d75
Daniel JCH <danielj.callisayah@gmail.com>
2022-01-19 19:17:35 -0400

    Polish app

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -12,6 +12,7 @@ struct AddView: View {
     @State private var type = "Personal"
     @State private var amount = 0.0
     @ObservedObject var expenses: Expenses
+    @Environment(\.dismiss) var dismiss
     
     let types = ["Business", "Personal"]
     
@@ -34,6 +35,7 @@ struct AddView: View {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
+                    dismiss()
                 }
             }
         }
